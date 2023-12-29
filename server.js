dd76664ab9abcd2e50cda19157ae095f3fbe603b
Chris Hundhausen <hundhaus@wsu.edu>
2023-12-29 13:19:16 -0800

    Added functions to read and write database, along with starting database file

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
 const express = require('express');
 const app = express();
+const fs = require('fs'); 
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+const userFile = './users.json';
+
+/*************************************************************************
+ * @function readUsers
+ * @desc Reads the users.json file and stores the contents in 
+ *      app.locals.users. If the file cannot be read, app.locals.users
+ *     is set to null.
+ * @param none
+ * @return users object containing database if read was successful,
+ *        null otherwise.
+ * **********************************************************************/
+function readUsers() {
+  try {
+    const data = fs.readFileSync(userFile);
+    const users = JSON.parse(data);
+    console.log("Users database successfully read from users.json file.");
+    return users;
+  } catch (err) {
+    console.log("Error reading database from users.json file: " + err.message);
+    return null;
+  }
+}
+
+/*************************************************************************
+ * @function writeUsers
+ * @desc Writes the database to the users.json file.
+ * @param users, the users object to be written to the file
+ * @return {boolean} true if the write was successful, false otherwise.
+ * **********************************************************************/
+function writeUsers(users) {
+  try {
+    fs.writeFileSync(userFile, JSON.stringify(users));
+    console.log("Users database successfully written to file.");
+    return true;
+  } catch (err) {
+    console.log("Error writing users database to file: " + err.message);
+    return false;
+  }
+}
+
+app.locals.users = readUsers(); //Initialize users database from file
 
 app.use((err, req, res, next) => {
   if (err) {
