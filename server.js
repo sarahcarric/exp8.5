cfbd21a2fcf59e6956aab7d270a3d0c7aba214ae
Chris Hundhausen <hundhaus@wsu.edu>
2023-12-30 08:40:18 -0800

    Added conditional execution of app based on whether database loaded successfully

--- server.js
+++ server.js
@@ -53,4 +53,9 @@ app.use((err, req, res, next) => {
   }
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+/* Listen for requests on port */
+if (app.locals.users) {
+  app.listen(port, () => console.log(`Listening on port ${port}...`));
+} else {
+  console.log("Server not started. Users database could not be read.");
+}
